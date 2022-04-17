2d1e71720e6d0a5c3f15e0d0d7a051c20dd66663
Finn St John <stjohn.finn@gmail.com>
2022-04-17 00:57:36 -0700

    worked on the direction stuff and the cooldown and design

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import axios from 'axios';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import styles from './style'
@@ -9,9 +9,16 @@ import { GOOGLE_API_KEY } from '@env'
 import { useEffect, useState } from 'react';
 
 import { Magnetometer } from 'expo-sensors';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const DESTINATION_DEGREE = 0;
 const UPDATE_INTERVAL = 1000;
+const IMAGE_PATH = './assets/default_image.png';
+const D_LAT = 120.12153;
+const D_LONG = 40.124992;
+const COOLDOWN = 5;
+
+let cooldownStarted = false;
 
 function DirectorScreen() {
   const [deviceRotation, setDeviceRotation] = useState({
@@ -20,25 +27,35 @@ function DirectorScreen() {
     z: 0
   });
   const [angle, setAngle] = useState(0);
-  const [direction, setDirection] = useState('X');
-  const [antiDirection, setAntiDirection] = useState(0);
   const [arrowStyle, setArrowStyle] = useState({
     transform: [{rotate: `0deg`}],
-    fontSize: 200
+    fontSize: 200,
   });
 
-  // Magnetometer.setUpdateInterval(UPDATE_INTERVAL);
-  // Magnetometer.addListener(result => {
-  //   const _angle = calcAngle(result);
-  //   const _degree = calcDegree(_angle);
+  const [cooldown, setCooldown] = useState(0);
+  if (!cooldownStarted) {
+    cooldownStarted = true;
+    setInterval(() => {
+      if (cooldown > 0) {
+        setCooldown(prev => prev - 1);
+      }
+    }, 1000);
+  }
 
-  //   setDeviceRotation(result);
-  //   setAngle(_angle);
-  //   setArrowStyle({
-  //     transform: [{rotate: `${-_degree + DESTINATION_DEGREE}deg`}],
-  //     fontSize: 200
-  //   });
-  // });
+  const updateAngle = async () => {
+    console.log('updating...');
+    const result = await getMagnetometerResults();
+    console.log(result);
+    const _angle = calcAngle(result);
+    const _degree = calcDegree(_angle);
+
+    setDeviceRotation(result);
+    setAngle(_angle);
+    setArrowStyle({
+      transform: [{rotate: `${-_degree + DESTINATION_DEGREE}deg`}],
+      fontSize: 200
+    });
+  }
 
   const calcAngle = (magnetometer) => {
     let angle = 0;
@@ -57,10 +74,6 @@ function DirectorScreen() {
     return magnetometer - 90 >= 0 ? magnetometer - 90 : magnetometer + 271;
   };
 
-  useEffect(() => {
-
-  });
-
   return (
     <View style={styles.container}>
       <Text style={styles.toolTip} >Device must be horizontally level in order to show the correct direction.</Text>
@@ -69,6 +82,9 @@ function DirectorScreen() {
       </View>
       <Text style={styles.subtitle} >Distance:</Text>
       <Text style={styles.primary}>00.00m</Text>
+      <Pressable style={styles.refreshButton} onPress={() => {setCooldown(COOLDOWN); updateAngle()}} disabled={cooldown > 0} >
+        <Text style={cooldown > 0 ? styles.disabledButtonText : styles.buttonText}>{cooldown > 0 ? cooldown : 'Refresh'}</Text>
+      </Pressable>
     </View>
   )
 }
@@ -76,8 +92,9 @@ function DirectorScreen() {
 function PictureScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.subtitle}>Your clue image.</Text>
-      <Image source={require('./assets/default_image.jpeg')} style={styles.clueImage}></Image>
+      <Text style={styles.questText}>This picture, pulled from Google Maps StreetView, is your visual clue of the day. The hunt is on!</Text>
+      {/* <Text style={styles.subtitle}>Harnish Dalal</Text> */}
+      <Image source={require(IMAGE_PATH)} style={styles.clueImage}></Image>
     </View>
   )
 }
@@ -142,41 +159,72 @@ export default function App() {
                 iconName = focused ? 'image' : 'image-outline'
               }
 
-              return <Ionicons name={iconName} size={size} color={color} />;
+              return <Ionicons name={iconName} size={35} color={color} />;
             },
             tabBarActiveTintColor: 'white',
-            tabBarInactiveTintColor: 'gray',
+            tabBarInactiveTintColor: '#555',
             tabBarStyle: {
-              backgroundColor: '#333',
+              backgroundColor: '#222',
               borderTopWidth: 1,
-              borderTopColor: 'gray'
+              borderTopColor: 'gray',
+              height: 70,
             },
             tabBarItemStyle: {
               fontSize: 12
             },
             headerStyle: {
-              backgroundColor: '#333',
+              backgroundColor: '#222',
               borderBottomWidth: 1,
               borderBottomColor: 'gray',
+              height: 90
             },
             headerTitleStyle: {
               color: 'white',
-              fontSize: 24,
-              textAlign: 'center'
-            }
+              fontSize: 30,
+              textAlign: 'center',
+              fontFamily: 'Helvetica'
+            },
+            title: 'GLOBLE',
+            tabBarShowLabel: false,
           })}
         >
+        <Tab.Screen name="Clue" component={PictureScreen} 
+          options={{
+
+          }}
+        />
         <Tab.Screen name='Compass' component={DirectorScreen} 
           options={{
 
           }}
         />
-        <Tab.Screen name="Clue" component={PictureScreen} 
-          options={({ navigation }) => ({
-
-          })}
-        />
       </Tab.Navigator>
     </NavigationContainer>
   );
+}
+
+function calcDistance(u_lat, u_long, dest_lat, dest_long) {
+  return Math.sqrt((u_lat - dest_lat) * (u_lat - dest_lat) + (u_long - dest_long) * (u_long - dest_long));
+}
+
+function calcDirectionToDestination(u_lat, u_long, dest_lat, dest_long) {
+  let phi = 0;
+  let delta_x = dest_lat - u_lat;
+  let delta_y = dest_long - u_long;
+
+  if (delta_x > 0 && delta_y > 0) { phi = 0; }
+  else if (delta_x < 0 && delta_y > 0) { phi = 90; }
+  else if (delta_x < 0 && delta_y < 0) { phi = 180; }
+  else if (delta_x > 0 && delta_y < 0) { phi = 270; }
+
+  let theta = Math.atan(Math.abs(delta_y) / Math.abs(delta_x)) + phi;
+}
+
+async function getMagnetometerResults() {
+  let results;
+  await Magnetometer.addListener(res => {
+    results = res;
+  });
+  Magnetometer.removeAllListeners();
+  return results;
 }
--- style.js
+++ style.js
@@ -16,7 +16,7 @@ export default StyleSheet.create({
         fontSize: 48
     },
     subtitle: {
-        fontSize: 16,
+        fontSize: 18,
         color: GRAY
     },
     arrowContainer: {
@@ -26,11 +26,11 @@ export default StyleSheet.create({
         borderRadius: 215,
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: 100,
+        marginBottom: 30,
         marginTop: 10
     },
     toolTip: {
-        fontSize: 16,
+        fontSize: 18,
         textAlign: "center",
         width: 200,
         color: GRAY
@@ -40,6 +40,34 @@ export default StyleSheet.create({
         height: 200,
         margin: 10,
         borderWidth: 1,
-        borderColor: 'black'
+        borderColor: 'gray',
+        borderRadius: 5
+    },
+    questText: {
+        textAlign: 'center',
+        width: 200,
+        fontSize: 18,
+        color: GRAY,
+        margin: 10
+    },
+    refreshButton: {
+        borderWidth: 1,
+        padding: 10,
+        borderColor: GRAY,
+        height: 60,
+        width: 150,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30,
+        borderRadius: 4
+    },
+    buttonText: {
+        color: '#007AFF',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    disabledButtonText: {
+        color: GRAY,
+        fontSize: 24,
     }
 })
